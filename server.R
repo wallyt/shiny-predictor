acf027f99456119bb70a548020c7b1dcc9fa5039
Wally Thornton <wfthornton@gmail.com>
2015-11-12 11:37:11 -0600

    added RF accuracy, confusion matrix, ROC curve and AUC calc outputs

--- server.R
+++ server.R
@@ -1,7 +1,32 @@
 library(shiny)
+library(scales)
+library(randomForest)
+library(ROCR)
+
+training <- read.csv("data/training.csv")
+testing <- read.csv("data/testing.csv")
+
+# Test-run a random forest
+set.seed(42)
+fit <- randomForest(factor(Performance) ~ ., importance = T, data = training)
+preds <- predict(fit, testing)
+matrix <- confusionMatrix(testing$Performance, preds)
+
+pr <- predict(fit, testing, type = "prob")[,2]
+pred <- prediction(pr, testing$Performance)
+perf <- performance(pred, "tpr", "fpr")
+perfAUC <- performance(pred, "auc")
+auc <- perfAUC@y.values[[1]]
 
 shinyServer(
     function(input, output) {
         output$vars <- renderPrint({input$varID})
+        output$accur <- renderPrint({percent(matrix$overall[1])})
+        output$confuse <- renderPrint({matrix$table})
+        output$roc <- renderPlot({
+                plot(perf, main = "ROC Curve for Random Forest", col=2, lwd=2) 
+                abline(a=0,b=1,lwd=2,lty=2,col="gray") 
+                text(0.8,0.4,paste("AUC = ",format(auc, digits=5, scientific=FALSE)))
+        })
     }
 )
--- ui.R
+++ ui.R
@@ -11,11 +11,22 @@ shinyUI(pageWithSidebar(
                              "# Years Investing Experience" = "Years.Investing", 
                              "Credit Score" = "Credit.Score")
         ),
+        sliderInput('range', 'Adjust the range of (normalized) credit scores to be used', 
+                    value = min(training$Credit.Score), 
+                    min = min(training$Credit.Score), 
+                    max = max(training$Credit.Score), 
+                    step = 0.05),
         submitButton("Run it!")
     ),
     mainPanel(
         h3('Result of random forest model'),
-        h2("You've selected:"),
-        verbatimTextOutput("vars")
+        h4("You've selected:"),
+        verbatimTextOutput("vars"),
+        h4("Which resulted in an accuracy on the test set of:"),
+        verbatimTextOutput("accur"),
+        h4("With a confusion matrix of:"),
+        verbatimTextOutput("confuse"),
+        h4("ROC"),
+        plotOutput('roc')
     )
 ))
