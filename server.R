aca6ec3f96c9e847aa80a77a3e4b773a71c5b1b1
Wally Thornton <wfthornton@gmail.com>
2015-11-15 11:18:32 -0600

    model now functional

--- server.R
+++ server.R
@@ -1,32 +1,71 @@
 library(shiny)
+library(dplyr)
 library(scales)
 library(randomForest)
 library(ROCR)
+library(caret)
 
 training <- read.csv("data/training.csv")
+training$Performance <- factor(training$Performance, levels = c("Good", "Bad"))
 testing <- read.csv("data/testing.csv")
+testing$Performance <- factor(testing$Performance, levels = c("Good", "Bad"))
 
 # Test-run a random forest
-set.seed(42)
-fit <- randomForest(factor(Performance) ~ ., importance = T, data = training)
-preds <- predict(fit, testing)
-matrix <- confusionMatrix(testing$Performance, preds)
+forest <- function(toggle, credits, inputs) {
+    if(toggle == 1) {
+        train <- training %>% filter(Credit.Score >= credits[1], 
+                                    Credit.Score <= credits[2])
+        test <- testing %>% filter(Credit.Score >= credits[1], 
+                                    Credit.Score <= credits[2])
+    } else {
+        train <- training
+        test <- testing
+    }
+    
+    Formula <- formula(paste("factor(Performance) ~", 
+                             paste(inputs, collapse="+")))
+    set.seed(42)
+    fit <- randomForest(Formula, importance = T, data = train)
+    set.seed(42)
+    preds <- predict(fit, test)
+    matrix <- confusionMatrix(preds, test$Performance)
+    # Build ROC curve data and AUC value
+    set.seed(42)
+    pr <- predict(fit, test, type = "prob")[,1]
+    set.seed(42)
+    pred <- prediction(pr, test$Performance)
+    perf <- performance(pred, "tpr", "fpr")
+    perfAUC <- performance(pred, "auc")
+    auc <- perfAUC@y.values[[1]]
 
-pr <- predict(fit, testing, type = "prob")[,2]
-pred <- prediction(pr, testing$Performance)
-perf <- performance(pred, "tpr", "fpr")
-perfAUC <- performance(pred, "auc")
-auc <- perfAUC@y.values[[1]]
+    returnList <- list("matrix" = matrix, "perf" = perf, "auc" = auc)
+    return(returnList)
+}
 
 shinyServer(
     function(input, output) {
-        output$vars <- renderPrint({input$varID})
-        output$accur <- renderPrint({percent(matrix$overall[1])})
-        output$confuse <- renderPrint({matrix$table})
+        returnList <- reactive({
+            toggle <- ifelse("Credit.Score" %in% input$varID, 1, 0)
+            credits <- c(input$range[1], input$range[2])
+            factors <- input$varID
+            forest(toggle, credits, factors)
+        })
+        output$accur <- renderPrint({
+            returnList()
+            percent(returnList()$matrix$overall[1])
+        })
+        output$confuse <- renderPrint({returnList()$matrix$table})
         output$roc <- renderPlot({
-                plot(perf, main = "ROC Curve for Random Forest", col=2, lwd=2) 
-                abline(a=0,b=1,lwd=2,lty=2,col="gray") 
-                text(0.8,0.4,paste("AUC = ",format(auc, digits=5, scientific=FALSE)))
+            plot(returnList()$perf, main = "ROC Curve for Random Forest", col=2, lwd=2) 
+            abline(a=0,b=1,lwd=2,lty=2,col="gray") 
+            text(0.8,0.4,paste("AUC = ",format(returnList()$auc, digits=5, scientific=FALSE)))
+        })
+        
+        output$vars <- renderText({
+            paste(input$varID, " ")
+        })
+        output$`cr-range` <- renderText({
+            paste(input$range[1], "to", input$range[2])
         })
     }
 )
--- ui.R
+++ ui.R
@@ -1,27 +1,35 @@
 library(shiny)
 
+training <- read.csv("data/training.csv")
+testing <- read.csv("data/testing.csv")
+
 shinyUI(pageWithSidebar(
     headerPanel("Predicting Loan Performance"),
     sidebarPanel(
-        h3('Select the variables to be used in the model'),
-        checkboxGroupInput("varID", "Variables",
+        h4('Potential predictors of loan performance'),
+        br(),
+        checkboxGroupInput("varID", "Select the variables to be used in the model",
                            c("Original Loan Amount" = "Original.Balance",
                              "Loan to Value Ratio" = "Loan.to.Value", 
                              "Property Appraised Value" = "Appraised.Price",
                              "# Years Investing Experience" = "Years.Investing", 
-                             "Credit Score" = "Credit.Score")
+                             "Credit Score" = "Credit.Score"),
+                           selected = c("Loan to Value Ratio", "Credit Score")
         ),
-        sliderInput('range', 'Adjust the range of (normalized) credit scores to be used', 
-                    value = min(training$Credit.Score), 
+        br(),
+        sliderInput('range', 'Adjust the range of credit scores to be used', 
+                    value = c(min(training$Credit.Score), max(training$Credit.Score)),
                     min = min(training$Credit.Score), 
                     max = max(training$Credit.Score), 
-                    step = 0.05),
+                    step = 5),
         submitButton("Run it!")
     ),
     mainPanel(
         h3('Result of random forest model'),
         h4("You've selected:"),
         verbatimTextOutput("vars"),
+        h4("And a range of credit scores of:"),
+        verbatimTextOutput("cr-range"),
         h4("Which resulted in an accuracy on the test set of:"),
         verbatimTextOutput("accur"),
         h4("With a confusion matrix of:"),
